d4fb59fcddfce66ac70edb0224f808e87438c7b5
Sriharsha <24wh5a1202@bvrithyderabad.edu.in>
2025-08-16 12:10:07 +0530

    Updated App.tsx with new routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom"; // ✅ Changed to HashRouter
 import Index from "./pages/Index";
 import BlockView from "./pages/BlockView";
 import BuildingView from "./pages/BuildingView";
@@ -16,16 +16,16 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/dashboard/block/:blockId" element={<BlockView />} />
           <Route path="/dashboard/building/:buildingId" element={<BuildingView />} />
           <Route path="/dashboard/sensor/:sensorId" element={<SensorView />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          {/* Catch-all for 404s */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
